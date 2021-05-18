677aeff5f72af1d07d593512240848758e2ad059
Michael Rodriguez <mikeyrod22@gmail.com>
2021-05-18 13:45:42 -0500

    removed foreign key constraints from procut tag associations

--- models/index.js
+++ models/index.js
@@ -6,18 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, { foreignKey: 'category_id' });
+
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id' });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id'
-})
+  foreignKey: 'tag_id',
+  constraints: false
+});
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'product_id'
-})
+  foreignKey: 'product_id',
+  constraints: false
+});
+
 module.exports = {
   Product,
   Category,
