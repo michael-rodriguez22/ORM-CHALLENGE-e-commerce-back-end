e9861cfab68e3b56eaa5f8cf574915c6a4d62a43
Michael Rodriguez <mikeyrod22@gmail.com>
2021-05-17 22:48:38 -0500

    first product route created, product tag foreign keys updated

--- models/index.js
+++ models/index.js
@@ -5,16 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-ProductTag.belongsTo(Category, { foreignKey: 'category_id' });
+Product.belongsTo(Category, { foreignKey: 'category_id' });
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id' });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 module.exports = {
   Product,
