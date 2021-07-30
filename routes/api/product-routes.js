15b7e7169e7b968eb94922a5aa12bd4d059a2f5c
Michael Rodriguez <mikeyrod22@gmail.com>
2021-07-30 17:14:57 -0500

    all product routes fully functional

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -103,19 +103,11 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
-    where: { id: req.params.id },
-  })
+  ProductTag.destroy({ where: { product_id: req.params.id } });
+  Product.destroy({ where: { id: req.params.id } })
     .then((productData) => {
-      if (!productData) {
-        res.status(404).json({ message: "We could not find this product." });
-        return;
-      }
-      res
-        .status(200)
-        .json({
-          message: `Product ${req.params.id} has been successfully deleted.`,
-        });
+      if (!productData) return res.status(404).json({ message: "We could not find this product." });
+      else res.status(200).json({ message: `This product has been successfully deleted.` });
     })
     .catch((err) => res.status(500).json(err));
 });
