b891964bf5c728de486816daf6b5b928efe5217e
Michael Rodriguez <mikeyrod22@gmail.com>
2021-11-06 21:37:51 -0500

    update server.js
    test connection and sync models to db

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express")
+const routes = require("./routes")
+
 // import sequelize connection
+const sequelize = require("./config/connection")
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(routes);
+app.use(routes)
+
+// test connection
+try {
+  sequelize.authenticate()
+  console.log("Database connected...")
+} catch (err) {
+  console.log(err)
+}
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`)
+    })
+  })
+  .catch(err => console.log(err))
